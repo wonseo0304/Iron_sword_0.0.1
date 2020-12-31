b5dd6bab1c78d77287e7056859f2b0f0429bc1e7
wonseo0304 <58354093+wonseo0304@users.noreply.github.com>
2020-12-31 21:16:31 +0900

    Initial commit

---
+++ src/main/java/iron_sword/main/Main.java
@@ -0,0 +1,24 @@
+package iron_sword.main;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void Right_click_iron_sword(PlayerInteractEvent e){
+
+    }
+}
